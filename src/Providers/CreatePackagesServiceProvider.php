d52fd4afc9b93df0badf1f43fd3a97d8706f1405
Joren Van Hocht <jorenvanhocht@MacBook-Pro-van-Joren.local>
2015-08-23 15:27:57 +0200

    Add the start files for the package
    add empty config file
    add empty read me
    add empty artisan command
    add empty service provider

---
+++ src/Providers/CreatePackagesServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace jorenvanhocht\CreatePackages\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CreatePackagesServiceProvider extends ServiceProvider {
+
+    /**
+     * Register the service provider
+     *
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Load the resources
+     *
+     */
+    public function boot()
+    {
+
+    }
+
+}
