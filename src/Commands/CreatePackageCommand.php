48e2ba517f7ecee811d743a1418f2983257a1730
Joren Van Hocht <jorenvanhocht@MacBook-Pro-van-Joren.local>
2015-08-27 21:38:47 +0200

    Create missing classes and write output to the user

--- src/Commands/CreatePackageCommand.php
+++ src/Commands/CreatePackageCommand.php
@@ -72,11 +72,22 @@ class CreatePackageCommand extends Command
         // create empty service provider
         $this->createServiceProvider();
         // create empty facade
+        $this->createFacade();
         // create empty package class
+        $this->createPackageClass();
 
+        $this->info('Folder structure and classes sucesfully created');
         // return psr4 autoloading line for in composer.json file
+        $this->info('Add this line to the PRS-4 autoloading section in your composer.json file');
+        $this->info($this->getComposerJsonAutoloading());
         // return service provider for in config/app.php
+        $this->info('');
+        $this->info('Add this line to the providers array in config/app.php');
+        $this->info($this->getServiceProvider());
         // return facade for in config/app.php
+        $this->info('');
+        $this->info('Add this line to the aliases array in config/app.php');
+        $this->info($this->getFacade());
     }
 
     private function createFolderStructure()
@@ -88,7 +99,8 @@ class CreatePackageCommand extends Command
         $this->filesystem->makeDirectory($basePath, 0755, true);
         $this->filesystem->makeDirectory("$basePath/src", 0755, true);
         $this->filesystem->makeDirectory("$basePath/tests", 0755, true);
-        $this->filesystem->makeDirectory("$basePath/Providers", 0755, true);
+        $this->filesystem->makeDirectory("$basePath/src/Providers", 0755, true);
+        $this->filesystem->makeDirectory("$basePath/src/Facades", 0755, true);
     }
 
     /**
@@ -108,8 +120,44 @@ class CreatePackageCommand extends Command
         $content = file_get_contents(__DIR__.'/../../templates/ServiceProvider.txt');
         $content = str_replace('{{vendorname}}', $this->getVendorName(), $content);
         $content = str_replace('{{packageName}}', ucfirst($this->package).'ServicePovider', $content);
-        $file = $this->config->folder.'/'.$this->getVendorName().'/'.$this->package.'/Providers/'.ucfirst($this->package).'ServiceProvider.php';
+        $file = $this->config->folder.'/'.$this->getVendorName().'/'.$this->package.'/src/Providers/'.ucfirst($this->package).'ServiceProvider.php';
 
         file_put_contents($file, $content);
+    }
+
+    private function createFacade()
+    {
+        $content = file_get_contents(__DIR__.'/../../templates/Facade.txt');
+        $content = str_replace('{{vendorname}}', $this->getVendorName(), $content);
+        $content = str_replace('{{packageName}}', ucfirst($this->package).'Facade', $content);
+        $content = str_replace('{{packageName2}}', strtolower($this->package), $content);
+        $file = $this->config->folder.'/'.$this->getVendorName().'/'.$this->package.'/src/Facades/'.ucfirst($this->package).'Facade.php';
+
+        file_put_contents($file, $content);
+    }
+
+    private function createPackageClass()
+    {
+        $content = file_get_contents(__DIR__.'/../../templates/PackageClass.txt');
+        $content = str_replace('{{vendorname}}', $this->getVendorName(), $content);
+        $content = str_replace('{{packageName}}', ucfirst($this->package), $content);
+        $file = $this->config->folder.'/'.$this->getVendorName().'/'.$this->package.'/src/'.ucfirst($this->package).'.php';
+
+        file_put_contents($file, $content);
+    }
+
+    private function getComposerJsonAutoloading()
+    {
+        return '"'.$this->getVendorName().'\\\\'.ucfirst($this->package).'": "'.$this->config->folder.'/'.$this->getVendorName().'/'.$this->package.'/src/"';
+    }
+
+    private function getServiceProvider()
+    {
+        return $this->getVendorName().'\\'.ucfirst($this->package).'\Providers\\'.ucfirst($this->package).'ServiceProvider::class,';
+    }
+
+    private function getFacade()
+    {
+        return $this->getVendorName().'\\'.ucfirst($this->package).'\Facades\\'.ucfirst($this->package).'Facade::class,';
     }
 }
